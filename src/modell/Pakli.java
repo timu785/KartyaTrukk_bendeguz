13e45725ba726010740d45c7ac09eb6d29dd7d1f
Sajtos Timudzsin Gergő (Szf_N_2024_2026) <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2025-09-17 16:07:05 +0200

    asd

--- src/modell/Pakli.java
+++ src/modell/Pakli.java
@@ -3,11 +3,11 @@ package modell;
 import modell.Lap;
 
 public class Pakli {
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
+    public static final String RESET  = "\u001B[0m";
+    public static final String RED    = "\u001B[31m";
+    public static final String GREEN  = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
-    public static final String BLACK = "\u001B[30m";
+    public static final String BLACK  = "\u001B[30m";
     
     private Lap[] lapok = new Lap[21];
     
